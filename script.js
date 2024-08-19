f50b4e6edc6163ffb7ef528cfb098523b75018cd
atmak <atmakurunithesh911@gmail.com>
2024-08-19 22:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-<body>
-    <div>
-        <!-- Your content here -->
-    </div>
-    <script src="script.js"></script>
-</body>
+// Select the link
+var link = document.querySelector('a');
 
+// Add an event listener to the link
+link.addEventListener('click', function(event) {
+    // Prevent the link from being followed
+    event.preventDefault();
+
+    // Show an alert
+    alert('You are about to visit the YouTube link of the song!');
+});
